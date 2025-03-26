433f74c9b218e21dfe6574b3b3dfcbed5477287c
NeerajBapat1 <neerajbapat001@gmail.com>
2025-03-26 20:18:50 +0530

    Changes made to the auth.py file d=for Swagger AI taking only token

--- app/auth.py
+++ app/auth.py
@@ -1,49 +1,32 @@
 import jwt
+from jwt.exceptions import PyJWTError
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
-from datetime import datetime, timedelta, timezone
+import secrets
 from app import database, models, schemas
 
-SECRET_KEY = "mysecretkey"
+SECRET_KEY = secrets.token_hex(32) # Generate a secure secrets key
 ALGORITHM = "HS256"
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)): # earlier there was no db: Session which was causing problems in fetching the current users
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-        
-        user = db.query(models.User).filter(models.User.username == username).first() # Fetching the user from the database
-        if user is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
-        return user
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
-    except jwt.InvalidTokenError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-    
+ 
 def get_password_hash(password: str):
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_access_token(data: dict, expires_delta: timedelta):
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + expires_delta # datetime.utcnow() is deprecated so datetime.now(timezone.utc) is used
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+def create_access_token(data: dict) -> str:
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
 
 def register_user(db: Session, user: schemas.UserCreate):
     existing_user = db.query(models.User).filter(models.User.username == user.username).first()
     if existing_user:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail = "User already registered" )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail = "User already registered" )
     hashed_password = get_password_hash(user.password)
     db_user = models.User(username=user.username, hashed_password=hashed_password)
     db.add(db_user)
@@ -54,6 +37,22 @@ def register_user(db: Session, user: schemas.UserCreate):
 def login_user(db: Session, user: schemas.UserLogin):
     db_user = db.query(models.User).filter(models.User.username == user.username).first()
     if not db_user or not verify_password(user.password, db_user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-    access_token = create_access_token({"sub": db_user.username}, timedelta(minutes=30))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect Username or password")
+    access_token = create_access_token(data = {"sub": db_user.username, "user_id": db_user.id})
     return {"access_token": access_token, "token_type": "bearer"}
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)): # earlier there was no db: Session which was causing problems in fetching the current users
+    credentials_exception = HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = "Could not validate credentials", headers = {"WWW-Authenticate": "Bearer"})
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("user_id")
+        if not username or not user_id:
+            raise credentials_exception
+    except PyJWTError:
+        raise credentials_exception
+    
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if not user:
+        raise credentials_exception
+    return user
